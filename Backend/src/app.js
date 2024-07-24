e4092fe603b972ce49de35b5ba16a6d4a81df1fb
Ashish kumar <ashishchumber34@gmail.com>
2024-07-24 16:00:00 +0530

    change the cors policy

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -25,7 +25,7 @@ app.use(Cors({
          credentials: true
 }))
 app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", 'http://localhost:5173');
+        res.header("Access-Control-Allow-Origin", process.env.CORS_ORIGIN);
         res.header("Access-Control-Allow-Credentials", true);
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
         res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
